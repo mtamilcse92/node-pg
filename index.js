1b9dc730c853ba5196b59bbe80e51ae7e4bcbe2d
mtamilcse92 <mtamilcse92@gmail.com>
2016-08-16 14:56:32 +0530

    second commit

--- index.js
+++ index.js
@@ -6,33 +6,49 @@ var conString = "pg://postgres:55555@localhost:5432/database";
 var client = new pg.Client(conString);
 client.connect();
 
-app.post('/', function(req, res){
-client.query("CREATE TABLE IF NOT EXISTS emps(firstname varchar(64), lastname varchar(64))");
-res.send("table created");
+app.post('/', function(req, res) {
+    var query = client.query("CREATE TABLE IF NOT EXISTS emps(id int PRIMARY KEY NOT NULL, empName varchar(64), empGender varchar(64), empAge int, empSalary int)");
+    query.on('error', function(err) {
+        console.log(err)
+    });
+    res.send("table created");
 });
 
-app.post('/insert', function(req, res){
-client.query("INSERT INTO emps(firstname, lastname) values($1, $2)", ['aaa', 'bbb']);
-res.send("values inserted");
+app.post('/insert', function(req, res) {
+    var id = req.param('id');
+    var empName = req.param('empName');
+    var empGender = req.param('empGender');
+    var empAge = req.param('empAge');
+    var empSalary = req.param('empSalary');
+    var query = client.query("INSERT INTO emps(id, empName, empGender, empAge, empSalary) values($1, $2, $3, $4, $5)", [id, empName, empGender, empAge, empSalary]);
+    query.on('error', function(err) {
+        console.log(err)
+    });
+    res.send("values inserted");
 });
 
-app.get('/find', function(req, res){
-var query = client.query("SELECT * FROM  emps");
-query.on("row", function (row, result) {
-    result.addRow(row);
-    console.log(row);
-});
-query.on("end", function (result) {
-    console.log(JSON.stringify(result.rows, null, "    "));
-    res.send(JSON.stringify(result.rows, null, "    "));
-    client.end();
-});
+app.get('/find/:id', function(req, res) {
+    var id = req.param('id');
+    var query = client.query("SELECT * FROM  emps WHERE id = $1", [id]);
+    query.on('error', function(err) {
+        console.log(err)
+    });
+    query.on("row", function(row, result) {
+        result.addRow(row);
+        console.log(row);
+    });
+    query.on("end", function(result) {
+        console.log(JSON.stringify(result.rows, null, "    "));
+        res.send(JSON.stringify(result.rows, null, "    "));
+    });
 });
 
-app.delete('/table', function(req, res){
-client.query("DROP TABLE emps");
-res.send("table deleted")
-client.end();
+app.delete('/table', function(req, res) {
+    var query = client.query("DROP TABLE emps");
+    query.on('error', function(err) {
+        console.log(err)
+    });
+    res.send("table deleted");
 });
 
 app.listen(3031);
